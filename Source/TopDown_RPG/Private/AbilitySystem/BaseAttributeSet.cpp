5e411593f4df385b2d26514d7b377ef4f5fd1934
Derman Can Danisman <dermancanwow@gmail.com>
2024-02-07 20:23:46 +0300

    Created attributes, replicated them, created RepNotify functions for those variables.
    Learned about attribute accessors. Attribute Accessor is a MACRO for Getting or Setting the FGameplayAttributeData or its numerical value.

--- Source/TopDown_RPG/Private/AbilitySystem/BaseAttributeSet.cpp
+++ Source/TopDown_RPG/Private/AbilitySystem/BaseAttributeSet.cpp
@@ -2,4 +2,49 @@
 
 
 #include "AbilitySystem/BaseAttributeSet.h"
+#include "Net/UnrealNetwork.h"
 
+UBaseAttributeSet::UBaseAttributeSet()
+{
+	InitHealth(100.f);
+	InitMaxHealth(500.f);
+	InitMana(80.f);
+	InitMaxMana(100.f);
+}
+
+/** Marks variables as replicated */
+void UBaseAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	/** REPNOTIFY_Always means that if the value is set on the server, replicate it and on the client, that value will be updated and set.
+	Now compare that to on changed, which is the default option. With on changed if you set the value of health on the server and that value hasn't changed, 
+	then there will be no replication. This is a sort of optimization if you're setting it to the same value that it already has, 
+	there's no need to replicate it in most cases. */
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UBaseAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBaseAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UBaseAttributeSet, Mana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UBaseAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+}
+
+void UBaseAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UBaseAttributeSet, Health, OldHealth);
+}
+
+void UBaseAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UBaseAttributeSet, MaxHealth, OldMaxHealth);
+}
+
+void UBaseAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UBaseAttributeSet, Mana, OldMana);
+}
+
+void UBaseAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UBaseAttributeSet, MaxMana, OldMaxMana);
+}
--- Source/TopDown_RPG/Public/AbilitySystem/BaseAttributeSet.h
+++ Source/TopDown_RPG/Public/AbilitySystem/BaseAttributeSet.h
@@ -4,7 +4,15 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
 #include "BaseAttributeSet.generated.h"
+
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
 /**
  * 
@@ -14,4 +22,62 @@ class TOPDOWN_RPG_API UBaseAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
 	
+public:
+
+	UBaseAttributeSet();
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	/**
+	* Core Attribute Variables
+	*/
+
+	/** Rep Notifies are called automatically when a variable replicates, 
+	so when the server replicates the variable down to a client, 
+	the client will have the rep notify for that variable triggered as a result. */
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Core Attributes")
+	FGameplayAttributeData Health;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealth, Category = "Core Attributes")
+	FGameplayAttributeData MaxHealth;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Mana, Category = "Core Attributes")
+	FGameplayAttributeData Mana;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxMana, Category = "Core Attributes")
+	FGameplayAttributeData MaxMana;
+
+
+
+public:
+
+	/**
+	* Core Attribute Functions
+	*/
+
+	/** Rep Notifies can take zero arguments or they can take one argument and
+	if they take an argument it has to be of the type of the replicated variable. */
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldMana) const;
+
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
+
+public:
+
+	/**
+	* Core Attribute Getters
+	*/
+
+	/** MACRO for Getting or Setting the FGameplayAttributeData or its numerical value */
+	ATTRIBUTE_ACCESSORS(UBaseAttributeSet, Health);
+	ATTRIBUTE_ACCESSORS(UBaseAttributeSet, MaxHealth);
+	ATTRIBUTE_ACCESSORS(UBaseAttributeSet, Mana);
+	ATTRIBUTE_ACCESSORS(UBaseAttributeSet, MaxMana);
 };
